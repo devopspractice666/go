86365a7d9ec3a230e3fe70127a10b4098ed3d301
devopspractice666 <mskazachenok_3_zubovich@mail.ru>
2026-04-13 23:47:55 +0300

    Работа со вложенными интерфейсами, type assertion с проверками, type switch

--- embedding/main.go
+++ embedding/main.go
@@ -6,7 +6,7 @@ type Animal struct {
 	Name string
 }
 
-func (a *Animal) speak() string {
+func (a *Animal) Speak() string {
 	return "Animal speaks"
 }
 
@@ -15,12 +15,16 @@ func (a *Animal) Info() string {
 	return info
 }
 
+func (a *Animal) Move() string {
+	return a.Name + " moves!"
+}
+
 type Dog struct {
 	Animal
 	Breed string
 }
 
-func (d *Dog) speak() string {
+func (d *Dog) Speak() string {
 	return "Woof"
 }
 
@@ -40,12 +44,21 @@ func (c *Cat) Info() string {
 }
 
 type Speaker interface {
-	speak() string
+	Speak() string
 	Info() string
 }
 
 func MakeSound(s Speaker) {
-	fmt.Println(s.speak())
+	fmt.Println(s.Speak())
+}
+
+type Mover interface {
+	Move() string
+}
+
+type AnimalInterface interface {
+	Speaker
+	Mover
 }
 
 func main() {
@@ -60,9 +73,43 @@ func main() {
 		MakeSound(element)
 	}
 
+	var test AnimalInterface = &Animal{Name: "Leo"}
+	fmt.Println(test.Move())
+	fmt.Println(test.Speak())
+	fmt.Println(test.Info())
+
+	var s Speaker = &Dog{Animal: Animal{Name: "Charlie"}, Breed: "Ovcharka"}
+	puppy := s.(*Dog)
+	println("Собака с именем ", puppy.Name)
+
+	if dog2, ok := s.(*Cat); ok {
+		fmt.Println(dog2.Name, "это кот")
+	} else {
+		dog3, okay := s.(*Dog)
+		if okay {
+			fmt.Println("Все таки", dog3.Name, " это собака!")
+		}
+	}
+
+	var s2 Speaker = &Cat{Animal: Animal{Name: "Barsik"}, Color: "Gray"}
+	switch typeAnimal := s2.(type) {
+	case *Dog:
+		fmt.Println(typeAnimal.Name, " Это собака!")
+	case *Cat:
+		fmt.Println(typeAnimal.Name, " Это кот!")
+	default:
+		fmt.Println("Непонятно что это за животное!")
+	}
 }
 
+//v1.0
 //работа с интерфейсами, встраивание структур, реализация методов интерфейса
 //методы dog затеняют методы из родительской структуры
 //кот юзает метод speak от animal т.к не реализован свой
 //создан метод принимающий объект интерфейса, который выводит спик для каждой стркутуры свой
+
+//v2.0
+//можно создавать вложенные интерфейсы, создавая интерфейс, который содержит все методы вложенных
+//из интерфейса можно доставать тип обратно и работать уже как с объектом определенного типа
+//можно выяснить является ли определенным типом объект интерфейса через _,ok:=
+//можно проверять какому типу принадлежит интерфейс через name.(type)

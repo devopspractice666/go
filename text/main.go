8e7adef87402fe7dd6e8ac759446e50fabdf308a
devopspractice666 <mskazachenok_3_zubovich@mail.ru>
2026-04-28 15:19:57 +0300

    final version of first web application with json db

--- text/main.go
+++ text/main.go
@@ -1,128 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	. "myapp/note"
+	. "myapp/server"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
-
-type Serv struct {
-	list []*Note
-}
-
-func NewServer() *Serv {
-	data := GetFromJson()
-	list := data.Notes
-	return &Serv{list: list}
-}
-
-func (l Serv) GetAllNotes(w http.ResponseWriter, r *http.Request) {
-	listNote := GetFromJson()
-
-	b, err := json.MarshalIndent(listNote.Notes, "", " ")
-	if err != nil {
-		fmt.Println("Ошибка кодирования в json формат")
-	}
-	_, err = w.Write(b)
-	if err != nil {
-		fmt.Println("Ошибка записи в ответ json формата")
-	}
-}
-
-func (l Serv) GetNoteByText(w http.ResponseWriter, r *http.Request) {
-	listNote := GetNoteByText(mux.Vars(r)["text"])
-	if len(listNote) == 0 {
-		w.WriteHeader(404)
-		return
-	}
-	b, err := json.MarshalIndent(listNote, "", " ")
-	if err != nil {
-		fmt.Println("Ошибка кодирования в json формат")
-	}
-
-	_, err = w.Write(b)
-	if err != nil {
-		fmt.Println("Ошибка записи в ответ json формата")
-	}
-}
-
-func (l Serv) GetNoteByID(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(mux.Vars(r)["id"])
-	if err != nil {
-		w.WriteHeader(400)
-		return
-	}
-	note := GetNoteByID(id)
-	if note == nil {
-		w.WriteHeader(404)
-		return
-	}
-	b, err := json.Marshal(note)
-	if err != nil {
-		w.WriteHeader(500)
-		return
-	}
-	w.Write(b)
-}
-
-func (l Serv) AddNoteToDatabase(w http.ResponseWriter, r *http.Request) {
-	var data map[string]any
-	json.NewDecoder(r.Body).Decode(&data)
-	if _, ok := data["text"]; !ok {
-		w.WriteHeader(400)
-		return
-	}
-	if len(data) != 1 {
-		w.WriteHeader(400)
-		return
-	}
-	text := data["text"].(string)
-	AddNoteByText(text)
-	message := "Данные добавлены в базу успешно"
-	b, err := json.Marshal(message)
-	if err != nil {
-		w.WriteHeader(500)
-	} else {
-		w.WriteHeader(201)
-		w.Write(b)
-	}
-}
-
-func (l Serv) RemoveNoteFromDatabase(w http.ResponseWriter, r *http.Request) {
-	var data map[string]any
-	json.NewDecoder(r.Body).Decode(&data)
-	if _, ok := data["text"]; !ok {
-		w.WriteHeader(400)
-		return
-	}
-	if len(data) != 1 {
-		w.WriteHeader(400)
-		return
-	}
-	text := data["text"].(string)
-	RemoveNotesByText(text)
-	message := "Данные удалены из базы успешно (если они есть)"
-	b, err := json.Marshal(message)
-	if err != nil {
-		w.WriteHeader(500)
-	} else {
-		w.WriteHeader(204)
-		w.Write(b)
-	}
-}
-
-func (l Serv) ChangeText(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	old := vars["p1"]
-	new := vars["p2"]
-	ChangeNoteText(old, new)
-	w.WriteHeader(204)
-
-}
 
 func main() {
 	ser := NewServer()
